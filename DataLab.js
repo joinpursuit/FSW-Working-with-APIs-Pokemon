80ce61ff7547df27ac33dea04bc27ad7e7e8af52
Bpmauge <briahanamauge@pursuit.org>
2019-09-21 09:53:35 -0400

    battle updates

--- DataLab.js
+++ DataLab.js
@@ -11,7 +11,7 @@ let pic2 = document.createElement('img');
 pic2.setAttribute('id', 'poke2');
 
 const firstPokemon = async () => {
-    let first = Math.floor(Math.random() * (807 - 0));
+    let first = Math.floor(Math.random() * (810 - 1) + 1);
     console.log(first)
     let p1 = `https://pokeapi.co/api/v2/pokemon/${first}`;
     let div1 = document.querySelector('div')
@@ -20,7 +20,6 @@ const firstPokemon = async () => {
     await axios.get(p1).then((response) => {
         try {
             let pokemon1 = response.data;
-            console.log(pokemon1.name, pokemon1.moves[0].move.name, pokemon1.moves[1].move.name, pokemon1.moves[2].move.name, pokemon1.moves[3].move.name);
 
             let data = document.querySelector('#data1');
 
@@ -34,9 +33,14 @@ const firstPokemon = async () => {
 
                 let name1 = document.createElement('h3');
                 name1.innerText = pokemon1.name;
+                name1.setAttribute('id', 'name1');
                 data1.appendChild(name1);
-
                 div1.appendChild(data1)
+
+                let hp1 = document.createElement('p');
+                hp1.innerText = `HP: ${pokemon1.stats[5].base_stat}`;
+                data1.appendChild(hp1);
+
                 for (i = 0; i < 4; i++) {
                     let move = document.createElement('p'); 0
                     move.innerText = `${moves[i].move.name} PP: `;
@@ -64,9 +68,14 @@ const firstPokemon = async () => {
 
                 let name1 = document.createElement('h3');
                 name1.innerText = pokemon1.name;
+                name1.setAttribute('id', 'name1');
                 newData1.appendChild(name1);
-
                 div1.appendChild(data1)
+
+                let hp1 = document.createElement('p');
+                hp1.innerText = `HP: ${pokemon1.stats[5].base_stat}`;
+                newData1.appendChild(hp1);
+
                 for (i = 0; i < 4; i++) {
                     let move = document.createElement('p');
                     move.innerText = `${moves[i].move.name} PP: `;
@@ -90,7 +99,7 @@ const firstPokemon = async () => {
 
 const secondPokemon = async () => {
 
-    let second = Math.floor(Math.random() * (807 - 0));
+    let second = Math.floor(Math.random() * (810 - 1) + 1);
     console.log(second)
     let p2 = `https://pokeapi.co/api/v2/pokemon/${second}`;
     let div2 = document.querySelector('div')
@@ -111,10 +120,16 @@ const secondPokemon = async () => {
                 data2.appendChild(pic2);
 
                 let name2 = document.createElement('h3');
+                name2.setAttribute('id', 'name2');
                 name2.innerText = pokemon2.name;
                 data2.appendChild(name2);
                 console.log(pokemon2)
                 div2.appendChild(data2)
+
+                let hp2 = document.createElement('p');
+                hp2.innerText = `HP: ${pokemon2.stats[5].base_stat}`;
+                data2.appendChild(hp2);
+
                 for (i = 0; i < 4; i++) {
                     let move = document.createElement('p');
                     move.innerText = `${moves[i].move.name} PP: `;
@@ -142,9 +157,14 @@ const secondPokemon = async () => {
 
                 let name2 = document.createElement('h3');
                 name2.innerText = pokemon2.name;
+                name2.setAttribute('id', 'name2');
                 newData2.appendChild(name2);
-
                 div2.appendChild(data2)
+
+                let hp2 = document.createElement('p');
+                hp2.innerText = `HP: ${pokemon2.stats[5].base_stat}`;
+                newData2.appendChild(hp2);
+
                 for (i = 0; i < 4; i++) {
                     let move = document.createElement('p');
                     move.innerText = `${moves[i].move.name} PP: `;
@@ -173,5 +193,24 @@ const get2Pokemon = () => {
 
 
 const battlePokemon = () => {
+    let num = Math.floor(Math.random() * 2);
+    console.log(num)
+    let firstPokemon = document.querySelector('#name1').innerText;
+    let secondPokemon = document.querySelector('#name2').innerText;
+    let totalHistory = document.querySelector('.battleHistory');
+
+    if (num === 0) {
+        let win1 = document.createElement('p');
+        win1.innerText = `${firstPokemon} defeats ${secondPokemon}!`;
+        totalHistory.appendChild(win1);
+
+     } else{
+         let win2 = document.createElement('p');
+         win2.innerText = `${secondPokemon} defeats ${firstPokemon}!`;
+         totalHistory.appendChild(win2);
+     }
 
 }
+
+
+       
