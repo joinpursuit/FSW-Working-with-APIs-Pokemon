d167b7253a795a04a052cea37f47ef3a0ae7277d
Owen Jones <owenthefirst93@gmail.com>
2019-09-21 11:17:54 -0400

    clarified code and fixed function/variable names

--- pokemon_battle_sim.js
+++ pokemon_battle_sim.js
@@ -1,55 +1,63 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let loadButton = document.querySelector('#loads-pokemon')
-    let battleButton = document.querySelector('#battle-pokemon')
+    const loadButton = document.querySelector('#loads-pokemon')
+    const battleButton = document.querySelector('#battle-pokemon')
 
     loadButton.addEventListener('click',gameSetup)
     battleButton.addEventListener('click',battlePokemon)
+    gameSetup()
 })
-
+//Main Function: gives us all pokemon data
  const getPokemon = async () => {
     const mainUrl = "https://pokeapi.co/api/v2/pokemon/?limit=964"
 
-    const randomNum = Math.floor((Math.random() * 964 -1) + 1)
-    console.log('random Num:',randomNum)
+    const indexForPokemonList = Math.floor((Math.random() * 964 -1) + 1)
+    console.log('random Num:',indexForPokemonList)
 
      const response = await axios.get(mainUrl)
-     let pokemonList = response.data.results
+     const pokemonList = response.data.results
      console.log('pokemon list:',pokemonList)
 
-    let battlePokemonURL = pokemonList[randomNum].url
+    const battlePokemonURL = pokemonList[indexForPokemonList].url
     console.log('battlePK URL:',battlePokemonURL)   
     
     const response2 = await axios.get(battlePokemonURL)
-    const pokemonAttributes = response2.data
-    console.log('pokemon Attributes: ',pokemonAttributes)
-    
-    const pokemonName = pokemonAttributes.name
-    console.log(pokemonName)
-    const pokemonSprite = pokemonAttributes.sprites.front_default
-    console.log(pokemonAttributes)
-    const pokemonBaseHP = pokemonAttributes.stats[5].base_stat
-    const pokemonMoves = pokemonAttributes.moves 
+    const chosenPokemonAttributes = response2.data
+    console.log('pokemon Attributes: ',chosenPokemonAttributes)
 
+    //Gets Pokemon Name, Sprite, and HP
+    const pokemonName = chosenPokemonAttributes.name
+    console.log(pokemonName)
+
+    const pokemonSprite = chosenPokemonAttributes.sprites.front_default
+    console.log(chosenPokemonAttributes)
+
+    const pokemonBaseHP = chosenPokemonAttributes.stats[5].base_stat
+    const pokemonMoves = chosenPokemonAttributes.moves 
+
+    //Function Gets 4 Pokemon Moves randomly and makes a request to get Name/PowerPoints
     const getPokemonMoves = async () => {
-        let movesArr = []
+
+        //this will hold the move urls
+        const movesArr = []
         
-        const anotherRandomNum = () => { return Math.floor((Math.random() * pokemonMoves.length -1) + 1)}
+        const getsPokemonMoveIndex = () => { return Math.floor((Math.random() * pokemonMoves.length -1) + 1)}
 
         if (pokemonMoves.length >= 4)  {
             for (let i=0; i < 4; i++){
-                movesArr.push(pokemonMoves[anotherRandomNum()].move.url)
+                movesArr.push(pokemonMoves[getsPokemonMoveIndex()].move.url)
             }
         } else if (pokemonMoves.length < 4){
             for (let i=0; i < pokemonMoves.length; i++){
-                movesArr.push(pokemonMoves[anotherRandomNum()].move.url)
+                movesArr.push(pokemonMoves[getsPokemonMoveIndex()].move.url)
             }
         }
-    
+        //Goes into each Move URL by accessing moves array 
         let urlForMove1 = movesArr[0]
         let urlForMove2 = movesArr[1]
         let urlForMove3 = movesArr[2]
         let urlForMove4 = movesArr[3]
 
+        //Makes a request to get name and PP for each attack
         const responseForURL1 = await axios.get(urlForMove1)
         const attackMove1 = responseForURL1.data
         const nameForMove1 = attackMove1.name
@@ -70,7 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const nameForMove4 = attackMove4.name
         const powerPointsForMove4 = attackMove4.pp
 
-        // [powerPointsForMove1, powerPointsForMove2, powerPointsForMove3, powerPointsForMove4]
+        // combine move name and pp in an array 
 
         const pokemonAttack1 = [nameForMove1 + ': ' + powerPointsForMove1].toString()
         const pokemonAttack2 = [nameForMove2 + ': ' + powerPointsForMove2].toString()
@@ -82,13 +90,14 @@ document.addEventListener('DOMContentLoaded', () => {
         return [pokemonAttack1, pokemonAttack2, pokemonAttack3, pokemonAttack4]
 
     }
-    let pokemon = getPokemonMoves()
 
-    let pokemonTings = await pokemon
+    //call function that gets moves randomly and helps with promise
+    const callerPokemonFunction = getPokemonMoves()
 
+    const pokemonTings = await callerPokemonFunction
     console.log(pokemonTings)
-
     
+    //DOM Manipulation
     const dataDiv = document.querySelector('#data')
     console.log(dataDiv)
 
@@ -115,6 +124,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
   
 }
+//handler that gives us two pokemon and clears DOM
 const gameSetup = () => {
     let divContainer = document.querySelector('#data')
     
@@ -129,19 +139,20 @@ const gameSetup = () => {
     }
 }
 
+//Picks a winner at random 
 const battlePokemon = () => {
     let divData = document.querySelector('#data')
-    let picker = Math.random()
+    const picker = Math.random()
     console.log(picker)
 
     if (picker > .49) {
-        let winnerText = document.createElement('p')
+        const winnerText = document.createElement('p')
         winnerText.innerText = 'Pokemon 1 wins'
         divData.appendChild(winnerText)
         window.alert('Pokemon 1 wins')
     
-    } else if (picker <= .50){
-        let winnerText = document.createElement('p')
+    } else if (picker < .50){
+        const winnerText = document.createElement('p')
         winnerText.innerText = 'Pokemon 2 wins'
         divData.appendChild(winnerText)
         window.alert('Pokemon 2 wins')
