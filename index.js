03ff39ef391907077dcb41e3db8d2fb4e32aed24
bsihame <sihamebazi@pursuit.org>
2019-09-21 18:09:53 -0400

    lab done

--- index.js
+++ index.js
@@ -1,65 +1,80 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('dom loaded')
-  // setupGetPokemonButton()
-  console.log("getButton")
-// const setupGetPokemonButton = () => {
-    let  getButton = document.querySelector('.button-pokemon')
-    getButton.addEventListener('click', getPokemon)
-      console.log("get pokemon was clicked")
+document.addEventListener("DOMContentLoaded", () => {
+  let getButton = document.querySelector(".button-pokemon");
 
-    
-  // }
-})
-
-function getRandom(min, max) {
-    return Math.floor(Math.random()*(809-1)+1);
-}
-
-  let p1Name = null;
-  let p1SpriteUrl = null;
-  let p1Hp = null;
-  let p2Name = null;
-  let p2SpriteUrl = null;
-  let p2Hp = null;
-function getPokemon() {
-  let randomId = getRandom()
-  fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)    
-  .then(response => {
-      console.log("response", response)
-      return response.json();
-  })
-  .then(data=> {
-        console.log("pokemon", data)
-        p1Name = document.querySelector(".pokemon-name1")
-        p1SpriteUrl = document.createElement("image")
-        p1Hp = document.querySelector('.hp')
-        P1HP.innerText = pokemon.
-
-        data.Hp
-        p2Name = data.name
-        p2SpriteUrl = data.sprites.front_default
-        p2Hp = data.hp
-        p1Name.innerText = pokemon.name
-        p1SprinteUrl.src = pokemon.sprites.front_default
-        displayPokemon(data);
-  })
-  .catch(err => {
-        console.log("err", err)
-      }) 
-}      
-      
-      function displayPokemon(pokemon){
-console.log(pokemon)
-      }
-
-
-
-//         return Math.floor(Math.random()*(809-1)+1);
-//       }
-//       function getPokemon(){
-//         let randomPokemon = fetch("https://pokeapi.co/api/v2/pokemon/{ function?????}/")
-//       }
+  getPokemon(1);
+  getPokemon(2);
   
-// const setupBattleButton = () => {
-//   let battleButton = document.querySelector('.battle')
-//   battleButton.addEventListener('click', (event))}
+  getButton.addEventListener("click", () => {
+    getPokemon(1);
+    getPokemon(2);
+  });
+
+  let battleButton = document.querySelector(".battle");
+
+  battleButton.addEventListener("click", selectWinner);
+
+  function selectWinner() {
+    let p1Name = document.querySelector("#p1Name").innerText;
+    let p2Name = document.querySelector("#p2Name").innerText;
+    let twoPokemon = [p1Name, p2Name];
+    let winner = twoPokemon[Math.floor(Math.random() * twoPokemon.length)];
+    let loser;
+    if (winner == twoPokemon[0]) {
+      loser = twoPokemon[1];
+    } else {
+      loser = twoPokemon[0];
+    }
+    let p = document.createElement("p");
+    p.innerText = `${winner} defeated ${loser}`;
+    let winners = document.querySelector(".battleHistory");
+    winners.appendChild(p);
+  }
+
+  function getPokemon(num) {
+    let randomId = getRandom();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+      .then(response => {
+        console.log("response", response);
+        return response.json();
+      })
+      .then(pokemonData => {
+        console.log("pokemon", pokemonData);
+
+        getMoves(pokemonData.moves, num);
+
+        document.querySelector("#p" + num + "Name").innerText =
+          pokemonData.name;
+        document.querySelector("#p" + num + "Img").src =
+          pokemonData.sprites.front_default;
+        document.querySelector("#p" + num + "Hp").innerText =
+          "Health Points: " + pokemonData.stats[5].base_stat;
+      })
+      .catch(err => {
+        console.log("err", err);
+      });
+  }
+
+  function getRandom() {
+    return Math.floor(Math.random() * (809 - 1) + 1);
+  }
+
+  function getMoves(arr, num) {
+    let ul = document.querySelector("#p" + num + "Moves");
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+    for (let i = 0; i < 4; i++) {
+      fetch(arr[i].move.url)
+        .then(response => {
+          return response.json();
+        })
+        .then(moveData => {
+          let li = document.createElement("li");
+          let moveName = moveData.name;
+          let movePP = moveData.pp;
+          li.innerText = `${moveName} PP: ${movePP}`;
+          ul.appendChild(li);
+        });
+    }
+  }
+});
