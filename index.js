e5f4a2706407b942dce07996927274355a757e92
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-09-21 10:25:05 -0400

    v0.6, data retrieval sys

--- index.js
+++ index.js
@@ -7,44 +7,68 @@ let pokemonRight = [];
 /* DOM-Loaded executes */
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#button-chooser').addEventListener('click', () => {
-        getDataFromApi();
+        getPokemon();
     });
 });
 
-const randomNumGen = (max) => {
-  return Math.floor(Math.random() * max) + 1
-}
+
 
 const handleError = (error) => {
   console.log(`The error that was thrown: `, error);
 }
 
-function getPokemon(whichOne) {
-
+const randomNumGen = (max) => {
+  return Math.floor(Math.random() * max) + 1
 }
 
-function battlePokemon() {
-
-}
-
-const getDataFromApi = async () => {
+const getMoveData = async (urlArray) => {
   try {
-    let outputPokemonObj = {
-      id: randomNumGen(151),
-      moveIndices: {}
-    };
-    let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${outputPokemonObj.id}/`);
-    // debugger;
-    // console.log(response.data);
-    outputPokemonObj['name'] = response.data.name;
-    outputPokemonObj['avatarUrl'] = response.data.sprites.front_default;
-    outputPokemonObj['baseHP'] = response.data.stats[5].base_stat;
-    while (Object.keys(outputPokemonObj.moveIndices).length < 4) {
-      outputPokemonObj.moveIndices[randomNumGen(response.data.moves.length - 1)] = true;
+    let movesArray = [];
+    for (let url of urlArray) {
+      let response = await axios.get(url);
+      let moveObj = {
+        name: response.data.name,
+        pp: response.data.pp
+      }
+      movesArray.push(moveObj);
     }
-    console.log(outputPokemonObj);
-
+    return movesArray;
   } catch (err) {
     handleError(err);
   }
+}
+
+const buildAPokemon = async () => {
+  try {
+    let outputPokemonObj = {
+      id: randomNumGen(151),
+      moveURLsObj: {},
+      moves: []
+    };
+    let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${outputPokemonObj.id}/`);
+    outputPokemonObj['name'] = response.data.name;
+    outputPokemonObj['avatarURL'] = response.data.sprites.front_default;
+    outputPokemonObj['baseHP'] = response.data.stats[5].base_stat;
+    while (Object.keys(outputPokemonObj.moveURLsObj).length < 4) {
+      let thisMoveIndex = randomNumGen(response.data.moves.length - 1);
+      outputPokemonObj.moveURLsObj[thisMoveIndex] = response.data.moves[thisMoveIndex].move.url;
+    } 
+    outputPokemonObj.moves = getMoveData(Object.values(outputPokemonObj.moveURLsObj));
+    return outputPokemonObj;
+  } catch (err) {
+    handleError(err);
+  }
+}
+
+const displayPokemon = (side, pokemonObj) => {
+  console.log(side, pokemonObj);
+}
+
+const getPokemon = () => {
+  displayPokemon('left', buildAPokemon());
+  displayPokemon('right', buildAPokemon());
+}
+
+const battlePokemon = () => {
+
 }
