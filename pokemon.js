1c714c8c243bc8a012267f20640400a4af607cfb
karenm09 <karenm929@hotmail.com>
2019-09-20 22:06:24 -0400

    still not working

--- pokemon.js
+++ pokemon.js
@@ -1,14 +1,27 @@
-let url = "https://pokeapi.co/api/v2/pokemon/?limit=482&offset=482/"
 document.addEventListener("DOMContendLoaded", () => {
     let button = document.querySelector('#get-pokemon')
     button.addEventListener('click', getPokemon)
     let button2 = document.querySelector('#battle')
     button2.addEventListener('click', battlePokemon)
-
 })
 
-const getPokemon = () => {
-    axios.get(url).then((response) => {
-        console.log(response.result)
-    })
+function getRandom() {
+    return Math.floor(Math.random() * (809 - 1) + 1);
 }
+const getPokemon = () => {
+
+    let randomId = getRandom()
+
+    let url = `http://pokeapi.co/api/v2/pokemon/${randomId}`
+
+    axios.get(url)
+        .then(response => {
+            let key = response.data.name
+            console.log("response", response)
+        })
+        .catch(error => {
+            console.log("there's an error", error)
+        })
+}
+
+
