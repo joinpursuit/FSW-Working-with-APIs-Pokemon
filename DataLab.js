a841815c745b490485e0a5a4c7fb102abe038053
Bpmauge <briahanamauge@pursuit.org>
2019-09-27 10:51:32 -0400

    shows type of pokemon on card and more styling

--- DataLab.js
+++ DataLab.js
@@ -1,92 +1,161 @@
 document.addEventListener('DOMContentLoaded', () => {
+    typeWriter();
+
     let button = document.querySelector('#get');
     button.addEventListener('click', get2Pokemon);
     let button2 = document.querySelector('#battle');
     button2.addEventListener('click', battlePokemon);
+
 })
 
-let pic1 = document.createElement('img');
-pic1.setAttribute('id', 'poke1');
-let pic2 = document.createElement('img');
-pic2.setAttribute('id', 'poke2');
+//make directions to load like a typewriter
+
+let txt = 'Click "Get Pokémon" to display your fighters. Click "Battle" to start the battle.';
+let speed = 100;
+let i = 0;
+let directions = document.createElement('p');
+
+function typeWriter() {
+    let instructions = document.querySelector('#instructions');
+    let text = "";
+    if (i < txt.length) {
+        text += txt.charAt(i);
+        directions.innerHTML += text;
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+    instructions.appendChild(directions);
+}
+
+
+const colortypes = {
+    fire: 'orangered',
+    water: 'lightskyblue',
+    grass: 'greenyellow',
+    electric: 'yellow',
+    psychic: 'lightcoral',
+    steel: 'silver',
+    bug: 'rgb(175, 255, 175)',
+    fairy: 'pink',
+    dark: 'darkgoldenrod',
+    flying: 'lavender',
+    ghost: 'orchid',
+    poison: 'magenta',
+    ice: 'lightcyan',
+    ground: 'beige',
+    rock: 'palegoldenrod',
+    dragon: 'plum',
+    fighting: 'palevioletred',
+    normal: 'olivedrab',
+
+}
+
 
 const firstPokemon = async () => {
+    let pic1 = document.createElement('img');
+    pic1.setAttribute('id', 'poke1');
     let first = Math.floor(Math.random() * (810 - 1) + 1);
-    console.log(first)
     let p1 = `https://pokeapi.co/api/v2/pokemon/${first}`;
-    let div1 = document.querySelector('div')
-
+    let div = document.querySelector('.data');
 
     await axios.get(p1).then((response) => {
         try {
             let pokemon1 = response.data;
-
-            let data = document.querySelector('#data1');
+            let data = document.querySelector('#pokeCard1');
 
             if (!data) {
-                let data1 = document.createElement('div');
-                data1.setAttribute('id', 'data1');
-                div1.appendChild(data1)
-                pic1.src = `${pokemon1.sprites.front_shiny}`;
-                let moves = pokemon1.moves;
-                data1.appendChild(pic1);
+                let pokeCard1 = document.createElement('div');
+                pokeCard1.setAttribute('id', 'pokeCard1')
+                div.appendChild(pokeCard1)
 
-                let name1 = document.createElement('h3');
+                let type = pokemon1.types[0].type.name;
+                console.log(type)
+                pokeCard1.style.backgroundColor = colortypes[type];
+
+                let typeName = document.createElement('h3'); 
+                typeName.innerText = `${pokemon1.types[0].type.name} `;
+                typeName.setAttribute('class', 'type1');
+                pokeCard1.appendChild(typeName)
+
+                let name1 = document.createElement('h2');
                 name1.innerText = pokemon1.name;
                 name1.setAttribute('id', 'name1');
-                data1.appendChild(name1);
-                div1.appendChild(data1)
+                pokeCard1.appendChild(name1);
+                div.appendChild(pokeCard1)
 
                 let hp1 = document.createElement('p');
                 hp1.innerText = `HP: ${pokemon1.stats[5].base_stat}`;
-                data1.appendChild(hp1);
+                hp1.setAttribute('id', 'hp1')
+                pokeCard1.appendChild(hp1);
+
+                pic1.src = `${pokemon1.sprites.front_shiny}`;
+                let moves = pokemon1.moves;
+                pokeCard1.appendChild(pic1);
+
+
+
+
 
                 for (i = 0; i < 4; i++) {
-                    let move = document.createElement('p'); 0
-                    move.innerText = `${moves[i].move.name} PP: `;
-                    data1.appendChild(move);
+                    let move = document.createElement('p');
+                    move.setAttribute('id', 'move')
+                    move.innerText = `${moves[i].move.name}`;
+                    pokeCard1.appendChild(move);
                     let moveURL = moves[i].move.url;
                     axios.get(moveURL).then((response) => {
                         let eachMove = document.createElement('p');
                         eachMove.setAttribute('class', 'power')
-                        eachMove.innerText = `   ${response.data.pp}`;
+                        eachMove.innerText = `PP: ${response.data.pp}`;
                         move.appendChild(eachMove)
-                        console.log(eachMove)
                     })
 
                 }
 
 
             } else {
-                newData1 = document.createElement('div');
-                newData1.setAttribute('id', 'data1');
-                data1.parentNode.replaceChild(newData1, data1)
+                newPokeCard1 = document.createElement('div');
+                newPokeCard1.setAttribute('id', 'pokeCard1');
+                pokeCard1.parentNode.replaceChild(newPokeCard1, pokeCard1)
 
-                pic1.src = `${pokemon1.sprites.front_shiny}`;
-                let moves = pokemon1.moves;
-                newData1.appendChild(pic1);
+                let type = pokemon1.types[0].type.name;
+                newPokeCard1.style.backgroundColor = colortypes[type];
+                console.log(type)
 
-                let name1 = document.createElement('h3');
+                let newTypeName = document.createElement('h3'); 
+                newTypeName.innerText = `${pokemon1.types[0].type.name} `;
+                newTypeName.setAttribute('class', 'type1');
+                newPokeCard1.appendChild(newTypeName)
+
+                let name1 = document.createElement('h2');
                 name1.innerText = pokemon1.name;
                 name1.setAttribute('id', 'name1');
-                newData1.appendChild(name1);
-                div1.appendChild(data1)
+                newPokeCard1.appendChild(name1);
+                div.appendChild(pokeCard1);
 
                 let hp1 = document.createElement('p');
                 hp1.innerText = `HP: ${pokemon1.stats[5].base_stat}`;
-                newData1.appendChild(hp1);
+                hp1.setAttribute('id', 'hp1')
+                newPokeCard1.appendChild(hp1);
+
+                pic1.src = `${pokemon1.sprites.front_shiny}`;
+                let moves = pokemon1.moves;
+                newPokeCard1.appendChild(pic1);
+
+
+
+
 
                 for (i = 0; i < 4; i++) {
                     let move = document.createElement('p');
-                    move.innerText = `${moves[i].move.name} PP: `;
-                    newData1.appendChild(move);
+                    move.innerText = `${moves[i].move.name}`;
+                    move.setAttribute('id', 'move')
+                    newPokeCard1.appendChild(move);
                     let moveURL = moves[i].move.url;
                     axios.get(moveURL).then((response) => {
                         let eachMove = document.createElement('p');
                         eachMove.setAttribute('class', 'power')
-                        eachMove.innerText = `   ${response.data.pp}`;
+                        eachMove.innerText = ` PP: ${response.data.pp}`;
                         move.appendChild(eachMove)
-                        console.log(eachMove)
                     })
                 }
             }
@@ -98,48 +167,63 @@ const firstPokemon = async () => {
 
 
 const secondPokemon = async () => {
-
+    let pic2 = document.createElement('img');
+    pic2.setAttribute('id', 'poke2');
     let second = Math.floor(Math.random() * (810 - 1) + 1);
     console.log(second)
     let p2 = `https://pokeapi.co/api/v2/pokemon/${second}`;
-    let div2 = document.querySelector('div')
+    let div = document.querySelector('.data')
 
     await axios.get(p2).then((response) => {
         try {
             let pokemon2 = response.data;
-            console.log(pokemon2.name, pokemon2.moves[0].move.name, pokemon2.moves[1].move.name, pokemon2.moves[2].move.name, pokemon2.moves[3].move.name);
-
-            let data = document.querySelector('#data2');
+            let data = document.querySelector('#pokeCard2');
 
             if (!data) {
-                let data2 = document.createElement('div');
-                data2.setAttribute('id', 'data2');
-                div2.appendChild(data2)
-                pic2.src = `${pokemon2.sprites.front_shiny}`;
-                let moves = pokemon2.moves;
-                data2.appendChild(pic2);
+                let pokeCard2 = document.createElement('div');
+                pokeCard2.setAttribute('id', 'pokeCard2');
+                div.appendChild(pokeCard2)
 
-                let name2 = document.createElement('h3');
+                let type = pokemon2.types[0].type.name;
+                console.log(type)
+                pokeCard2.style.backgroundColor = colortypes[type];
+
+                let typeName = document.createElement('h3'); 
+                typeName.innerText = pokemon2.types[0].type.name;
+                typeName.setAttribute('class', 'type2');
+                pokeCard2.appendChild(typeName)
+
+                let name2 = document.createElement('h2');
                 name2.setAttribute('id', 'name2');
-                name2.innerText = pokemon2.name;
-                data2.appendChild(name2);
-                console.log(pokemon2)
-                div2.appendChild(data2)
+                name2.innerText = `${pokemon2.name} `;
+                pokeCard2.appendChild(name2);
 
+                div.appendChild(pokeCard2)
                 let hp2 = document.createElement('p');
                 hp2.innerText = `HP: ${pokemon2.stats[5].base_stat}`;
-                data2.appendChild(hp2);
+                hp2.setAttribute('id', 'hp2')
+                pokeCard2.appendChild(hp2);
+
+
+                pic2.src = `${pokemon2.sprites.front_shiny}`;
+                let moves = pokemon2.moves;
+                pokeCard2.appendChild(pic2);
+
+
+
+
 
                 for (i = 0; i < 4; i++) {
                     let move = document.createElement('p');
-                    move.innerText = `${moves[i].move.name} PP: `;
-                    data2.appendChild(move);
+                    move.innerText = `${moves[i].move.name}`;
+                    move.setAttribute('id', 'move')
+                    pokeCard2.appendChild(move);
 
                     let moveURL = moves[i].move.url;
                     axios.get(moveURL).then((response) => {
                         let eachMove = document.createElement('p');
                         eachMove.setAttribute('class', 'power')
-                        eachMove.innerText = `   ${response.data.pp}`;
+                        eachMove.innerText = `PP: ${response.data.pp}`;
                         move.appendChild(eachMove)
                         console.log(eachMove)
                     })
@@ -147,37 +231,51 @@ const secondPokemon = async () => {
                 }
 
             } else {
-                newData2 = document.createElement('div');
-                newData2.setAttribute('id', 'data2');
-                data2.parentNode.replaceChild(newData2, data2)
+                newPokeCard2 = document.createElement('div');
+                newPokeCard2.setAttribute('id', 'pokeCard2');
+                pokeCard2.parentNode.replaceChild(newPokeCard2, pokeCard2)
 
-                pic2.src = `${pokemon2.sprites.front_shiny}`;
-                let moves = pokemon2.moves;
-                newData2.appendChild(pic2);
+                let type = pokemon2.types[0].type.name;
+                console.log(type)
+                newPokeCard2.style.backgroundColor = colortypes[type];
+                div.appendChild(newPokeCard2)
 
-                let name2 = document.createElement('h3');
+                let newTypeName = document.createElement('h3'); 
+                newTypeName.innerText = `${pokemon2.types[0].type.name} `;
+                newTypeName.setAttribute('class', 'type2');
+                newPokeCard2.appendChild(newTypeName)
+
+                let name2 = document.createElement('h2');
                 name2.innerText = pokemon2.name;
                 name2.setAttribute('id', 'name2');
-                newData2.appendChild(name2);
-                div2.appendChild(data2)
+                newPokeCard2.appendChild(name2);
 
                 let hp2 = document.createElement('p');
                 hp2.innerText = `HP: ${pokemon2.stats[5].base_stat}`;
-                newData2.appendChild(hp2);
+                hp2.setAttribute('id', 'hp2')
+                newPokeCard2.appendChild(hp2);
+
+
+                pic2.src = `${pokemon2.sprites.front_shiny}`;
+                let moves = pokemon2.moves;
+                newPokeCard2.appendChild(pic2);
+
+
+
+
 
                 for (i = 0; i < 4; i++) {
                     let move = document.createElement('p');
-                    move.innerText = `${moves[i].move.name} PP: `;
-                    newData2.appendChild(move);
+                    move.innerText = `${moves[i].move.name}`;
+                    move.setAttribute('id', 'move')
+                    newPokeCard2.appendChild(move);
                     let moveURL = moves[i].move.url;
                     axios.get(moveURL).then((response) => {
                         let eachMove = document.createElement('p');
                         eachMove.setAttribute('class', 'power')
-                        eachMove.innerText = `   ${response.data.pp}`;
+                        eachMove.innerText = ` PP: ${response.data.pp}`;
                         move.appendChild(eachMove)
-                        console.log(eachMove)
                     })
-
                 }
             }
         } catch (err) {
@@ -192,6 +290,8 @@ const get2Pokemon = () => {
 }
 
 
+
+
 const battlePokemon = () => {
     let num = Math.floor(Math.random() * 2);
     console.log(num)
@@ -204,13 +304,10 @@ const battlePokemon = () => {
         win1.innerText = `${firstPokemon} defeats ${secondPokemon}!`;
         totalHistory.appendChild(win1);
 
-     } else{
-         let win2 = document.createElement('p');
-         win2.innerText = `${secondPokemon} defeats ${firstPokemon}!`;
-         totalHistory.appendChild(win2);
-     }
+    } else {
+        let win2 = document.createElement('p');
+        win2.innerText = `${secondPokemon} defeats ${firstPokemon}!`;
+        totalHistory.appendChild(win2);
+    }
 
 }
-
-
-       
