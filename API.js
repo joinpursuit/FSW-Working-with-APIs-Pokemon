4a49d65071adfd667c24c6f5f471ab13312f92a1
kadjiah <kadijahwilson@pursuit.org>
2019-10-10 13:51:26 -0400

    Updating Pokemon

--- API.js
+++ API.js
@@ -1,94 +1,137 @@
 document.addEventListener("DOMContentLoaded", () => {
-setupButtonListeners();
-})
-
-// Listen for button clicks
-const setupButtonListeners = () => {
-    const getPokemonbutton = document.querySelector("#getPokemonbutton")
-    getPokemonbutton.addEventListener('click', fetchTwoRandomPokemon)
-
-    const startBattle = document.querySelector("#battleButton")
-    startBattle.addEventListener("click", startBattle)
-}
-
-const fetchTwoRandomPokemon = () => {
-    cleanArena();
-    fetchSinglePokemon();
-    fetchSinglePokemon();
-}
-
-const fetchSinglePokemon = () => {
-    const pokemonId = randomPokemon();
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`
-    fetch(url)
-    .then((response) => {
-        return response.json();
+    setupButtonListeners();
     })
-    .then((pokemonId) => {
-    let moves = pokemonId.moves
-
-    for(let move = 0; move < 5; move++){
-        let moveUrl = moves[move].move.url
-        fetchMove(moveUrl, pokemonId);
+    
+    // Listen for button clicks
+    const setupButtonListeners = () => {
+        const getPokemonbutton = document.querySelector("#getPokemonbutton")
+        getPokemonbutton.addEventListener('click', fetchTwoRandomPokemon)
+    
+        const startBattle = document.querySelector("#battleButton")
+        startBattle.addEventListener("click", startBattle)
     }
-    displayPokemon(pokemonId)
-    })
-}
-
-
-    const fetchMove = (url) => {
+    
+    const fetchTwoRandomPokemon = () => {
+        cleanArena();
+        fetchSinglePokemon();
+        fetchSinglePokemon();
+        
+    }
+    
+    const fetchSinglePokemon = () => {
+        const pokemonId = randomPokemon();
+        const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`
         fetch(url)
-        .then(response => response.json())
-            .then(move => {
-                console.log(move);
-                
-                // displayMove()
-            })
+        .then((response) => {
+            return response.json();
+        })
+        .then((pokemonId) => {
+        let moves = pokemonId.moves
+    // console.log(moves)
+    // console.log(pokemonId)
+        for(let move = 0; move < 5; move++){
+            let moveUrl = moves[move].move.url
+            // console.log(moveUrl)
+            fetchMove(moveUrl, pokemonId.name);
+        }
+        displayPokemon(pokemonId)
+        })
     }
-
-    const displayPokemon = (pokemon) => {
-        const pokemonCard = document.createElement('div');
-        pokemonCard.id = pokemon.name;
-        
-        const name = document.createElement('h2');
-        name.innerText = pokemon.name;
     
-        const sprite = document.createElement('img');
-        sprite.src = pokemon.sprites.front_default;
     
-        const hp = document.createElement('h3');
-        hp.innerText = pokemon.stats[5].base_stat;
-    
-        const moves = document.createElement('div');
-    
-        const movesHeader = document.createElement('h3');
-        movesHeader.innerText = "Moves: "
-    
-        const moveList = document.createElement('ul');
-        moveList.classList.add = ".moves-list"
-       
-        moves.appendChild(movesHeader)
-        moves.appendChild(moveList)
-        pokemonCard.appendChild(name)
-        pokemonCard.appendChild(sprite)
-        pokemonCard.appendChild(hp)
-        pokemonCard.appendChild(moves)
-        
-        const arena = document.querySelector('#arena')
-        arena.appendChild(pokemonCard)
+        const fetchMove = (url, pokemonName) => {
+            fetch(url)
+            .then(response => response.json())
+                .then(move => {
+                    // console.log(move);
+                    
+                    displayMove(move, pokemonName)
+                })
         }
     
-        const cleanArena = () => {
-            const arena = document.querySelector("#arena")
-            arena.innerHTML = ' ';
+        const displayPokemon = (pokemon) => {
+            const pokemonCard = document.createElement('div');
+            pokemonCard.id = pokemon.name;
+            
+            const name = document.createElement('h2');
+            name.innerText = pokemon.name;
+        
+            const sprite = document.createElement('img');
+            sprite.src = pokemon.sprites.front_default;
+        
+            const hp = document.createElement('h3');
+            hp.innerText = pokemon.stats[5].base_stat;
+        
+            const moves = document.createElement('div');
+            const movesHeader = document.createElement('h3');
+            movesHeader.innerText = "Moves: "
+        
+            const moveList = document.createElement('ul');
+            // moveList.classList.add = ".moves-list"
+            moveList.setAttribute("id", pokemon.name)
+           
+            moves.appendChild(movesHeader)
+            moves.appendChild(moveList)
+            pokemonCard.appendChild(name)
+            pokemonCard.appendChild(sprite)
+            pokemonCard.appendChild(hp)
+            pokemonCard.appendChild(moves)
+            
+            const arena = document.querySelector('#arena')
+            arena.appendChild(pokemonCard)
+            }
+        
+            const cleanArena = () => {
+                const arena = document.querySelector("#arena")
+                arena.innerText = ' ';
+            }
+        
+    
+    
+        const displayMove = (move, pokemonName) => {
+            let arena = document.querySelector(`#${pokemonName}`)
+            // console.log(pokemonName)
+            let movesLi = document.createElement('li')
+            
+            movesLi.innerText = move.name;
+    
+            arena.appendChild(movesLi);
+            
         }
     
-
+    const startBattle = () => {
+        let arena = document.querySelector(`#arena`)
+        let arr = []
+        let number = Math.floor((Math.random() * 2))
+        
+        let firstPokemon = arena.childNodes[1].id;
+        let secondPokemon = arena.childNodes[2].id;
+    
+        arr.push(firstPokemon, secondPokemon)
+        
+    
+        let winner = arr.splice(number, 1)[0]
+    
+        let defeated = arr[0]
+        
+      
+    
+        console.log(winner)
+        console.log(defeated)
+        
+        let footer = document.querySelector("#battleHistory")
+        let battleResults = document.createElement('p')
+        battleResults.innerText = winner + " deafeats " + defeated
+        
+        footer.appendChild(battleResults)
+        // console.log(battleResults)
+    }
+    
     const getRandomNumber = (MIN, MAX) => {
         return Math.floor(Math.random() * ((MAX - MIN) + MIN ));
         
-}
-
+    }
+    
     //range from 1 - 809 
     const randomPokemon = () =>  {
         const MAX = 809;
@@ -96,30 +139,3 @@ const fetchSinglePokemon = () => {
         const id = getRandomNumber(MIN, MAX + 1);
         return id;
     }
-
-    // const displayMove = (pokemon) => {
-        
-    // }
-
-function startBattle(){
-    let arena = document.querySelector(`#arena`)
-    let winner = pickWinner()
-
-    let defeated = winner == 1 ? 0 : 1
-    
-
-    let winnerPokemon = arena.childNodes[winner];
-    let defeatedPokemon = arena.childNodes[defeated];
-
-
-    let footer = document.querySelector("#BattleHistory")
-    let battleResults = document.createElement('p')
-    battleResults.innerText = winnerPokemon + " deafeats " + defeatedPokemon
-    
-    footer.appendChild(battleResults)
-}
-
-    const pickWinner = () => {
-    const winnerIndex = getRandomNumber(0, 2)
-    return winnerIndex
-}
