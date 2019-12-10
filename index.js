f3d7cc1445f601ebaa5ec534be4ebba31979dc40
Kevin Brutus <kevinbrutus@pursuit.org>
2019-12-09 21:50:29 -0500

    list two pokemon

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ document.addEventListener("DOMContentLoaded", () => {
             try {
                 let res = await axios.get("https://pokeapi.co/api/v2/pokemon/");
                 let pokemon = res.data.results
-                pokemon.forEach(poke => {
-                    let data = document.querySelector("#data");
-                    let info = document.createElement("P")
-                    info.innerText = poke.name;
-                    data.appendChild(info)
+                debugger
+                let poke = pokemon[Math.floor(Math.random()*pokemon.length)]
+                let poke2 = pokemon[Math.floor(Math.random()*pokemon.length)]
+                let data = document.querySelector("#data");
+                let info = document.createElement("P")
+                let info2 = document.createElement("P")
+                info.innerText = poke.name;
+                info2.innerText = poke2.name;
+                data.appendChild(info)
+                data.appendChild(info2)
 
-                })
 
             } 
             catch(err){
                 console.log(err)
             }
         }
-        getPokemon()
+    getPokemon()
     })
 })
 // console.log("hello")
