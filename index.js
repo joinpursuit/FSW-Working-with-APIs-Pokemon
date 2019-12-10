44800cedbcb9335a8c9fb757e1a54aa5ddc7bf81
Ohidurr <Ohidurrahman96@gmail.com>
2019-12-09 21:46:01 -0500

    add axios

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 document.addEventListener("DOMContentLoaded",() => {
-    let getPokemon = document.querySelector("button")
+    let pokeData = Math.floor((Math.random()*809)+1) 
+    let  pokeButton = document.querySelector("#get")
     
     const getPokemon = async () => {
         try{
-            let res = await axios.get("https://pokeapi.co/api/v2/pokemon")
+            let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeData}`)
+            let pokemon = res.data;
+            // pokemon.forEach(pokemon => {
+            //     debugger 
+            // })
+        }catch(err){
+            console.log("Error");
+            
         }
     }
+    pokeButton.addEventListener("click",()=>{
+
+        getPokemon()
+        getPokemon()
+    })
 })
+
+
+// https://pokeapi.co/docs/v2.html#pokemon
+// https://pokeapi.co/docs/v2.html/#moves
