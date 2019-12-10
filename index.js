ec8f48127d6126f624174ef0ba9f969510f44ba0
dannyl <danilovlaurent@pursuit.org>
2019-12-09 21:07:30 -0500

    finished html setup

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded",() => {
+    let getPokeButton = document.querySelector(".getPokeButton")
+    let battleButton = document.querySelector(".battleButton")
+
+    const getPokemon = () => {
+
+    }
+    const battlePokemon = () => {
+       
+    }
+
+    getPokeButton.addEventListener("click",() => {
+        
+        getPokemon()
+    })
+
+    battleButton.addEventListener("click", () => {
+        
+        battlePokemon()
+
+    })
+
+
+})
