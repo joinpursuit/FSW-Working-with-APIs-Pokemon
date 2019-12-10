8e60ff048316ae4d7c5cf77385412295431d514e
Kevin Brutus <kevinbrutus@pursuit.org>
2019-12-09 21:19:32 -0500

    getPokemon function

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     let get = document.querySelector("#get")
     get.addEventListener("click", () => {
+        const getPokemon = async () => {
+            try {
+                let res = await axios.get("https://pokeapi.co/api/v2/pokemon/");
+                let pokemon = res.data.results
+                pokemon.forEach(poke => {
+                    let data = document.querySelector("#data");
+                    let info = document.createElement("P")
+                    info.innerText = poke.name;
+                    data.appendChild(info)
 
+                })
+
+            } 
+            catch(err){
+                console.log(err)
+            }
+        }
+        getPokemon()
     })
 })
-console.log("hello")
+// console.log("hello")
