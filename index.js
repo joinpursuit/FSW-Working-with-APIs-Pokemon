48866072fa54540b24760ed469cf1441ba709ee6
Kevin Brutus <kevinbrutus@pursuit.org>
2019-12-10 20:33:24 -0500

    get pokemon sprites and organize to keep better track of pokemon

--- index.js
+++ index.js
@@ -7,18 +7,31 @@ document.addEventListener("DOMContentLoaded", () => {
             try {
                 let res = await axios.get("https://pokeapi.co/api/v2/pokemon/");
                 pokemonCount = res.data.count
+
+                // POKEMON 1
                 let pokeID = Math.floor((Math.random() * pokemonCount) + 1);
-                let pokeID2 = Math.floor((Math.random() * pokemonCount) + 1);
                 let random = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeID}/`)
-                let random2 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeID2}/`)
                 let challengerinfo = document.querySelector("#challengerinfo");
-                let championinfo = document.querySelector("#championinfo");
                 let info = document.createElement("li")
-                let info2 = document.createElement("li")
+                let sprite1 = document.createElement("img")
                 info.innerText = random.data.name;
-                info2.innerText = random2.data.name;
+                let src = random.data.sprites.front_default
+                sprite1.src = src
                 challengerinfo.appendChild(info)
+                challengerinfo.appendChild(sprite1)
+                
+                
+                // POKEMON 2
+                let pokeID2 = Math.floor((Math.random() * pokemonCount) + 1);
+                let random2 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeID2}/`)
+                let championinfo = document.querySelector("#championinfo");
+                let sprite2 = document.createElement("img")
+                let info2 = document.createElement("li")
+                let src2 = random2.data.sprites.front_default
+                sprite2.src = src2
+                info2.innerText = random2.data.name;
                 championinfo.appendChild(info2)
+                championinfo.appendChild(sprite2)
                 // let moves = await axios.get("https://pokeapi.co/api/v2/move")
             } 
             catch(err){
