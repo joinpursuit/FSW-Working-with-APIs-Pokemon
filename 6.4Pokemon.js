7ae7bf4703bb7144cb861fbd265325b5633c2b01
DejaLFlynn <dejaflynn@pursuit.org>
2019-12-10 21:41:05 -0500

    corrected two pokemon function

--- 6.4Pokemon.js
+++ 6.4Pokemon.js
@@ -5,11 +5,13 @@ document.addEventListener("DOMContentLoaded",()=>{
         try{
             let randomA = Math.floor(Math.random()* 964) +1
             let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomA}`)
-            let pokemonA = res.data.results
+            let pokemonA = res.name
             pokemonA.forEach(pokemon =>{
                 let poke = document.createElement("h2")
-                poke.innerText = pokemon.name;
-                getPokemon.appendChild(poke)
+                let div1 = document.querySelector("pokemonA")
+                div1.innerText = pokemon.name;
+                
+                pokemonA.appendChild(poke)
             })
 
         }catch(err){
@@ -17,10 +19,30 @@ document.addEventListener("DOMContentLoaded",()=>{
         }
     }
     const displayTwo=async()=>{
-        displayOne()
-        displayOne()
+        try{
+            let randomB = Math.floor(Math.random()*964)+1
+            let res2 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomB}`)
+            let pokemonB = res2.name
+            pokemonB.forEach(pokemon=>{
+                let poke2 = document.createElement("h2")
+                let div2 = document.querySelector("pokemonB")
+                div2.innerText = pokemon.name;
+                pokemonB.appendChild(poke2)
+                
+            })
+        }catch(err){
+            console.log("error")
+        }
+  
     }
-    displayTwo()
+    const get2Pokemon =async()=>{
+        displayOne()
+        displayTwo()
+    }
+    // const battlePokemon=async()=>{
+
+    // }
+    get2Pokemon()
     getPokemon.addEventListener("click", (event)=>{
 
     })
