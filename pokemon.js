2edc8f5563175a24b2715b6dd1fb93c30d75ec8c
maria liza <mariamartinez@pursuit.org>
2019-12-11 14:09:55 -0500

    begin poke game set up, get first pokemon on screen

--- pokemon.js
+++ pokemon.js
@@ -1,18 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let button = document.querySelector("button")
-    const getPokemon = async () => {
-        try {
-            let res = await axios.get("https://swapi.co/api/people")
-            let characters= res.data.results;
-            characters.forEach(character => {
-                let option = document.createElement("option")
-                option.innerText = character.name;
-                option.value = character.homeworld;
-                select.appendChild(option)
-            })
-        } catch(err) {
-           
-        }
-    
+  let button = document.querySelector("#getPokemon")
+//   let pokemonOne = document.createElement("p1")
+//   let pokemonTwo = document.createElement("p2")
+  const getPokemon = async () => {
+    try {
+      let num = Math.floor(Math.random() * 807)
+      let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
+    //   debugger
+      let character = res.data
+      let name1 = character.name
+      let pokemonOne = document.querySelector("p");
+      pokemonOne.innerText = name1
+      let sprite = pokemon.sprites.front_default
 
+      
+    } catch (err) {
+      console.log("ERROR")
+    //   debugger
+    }
+  }
+
+  button.addEventListener("click", () => {
+    getPokemon()
+  })
 })
