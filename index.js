de5f67685c8fd332ba0f01a870051d6872370a21
Kevin Brutus <kevinbrutus@pursuit.org>
2019-12-10 20:18:32 -0500

    turn paragraph style into list style

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 let pokeID2 = Math.floor((Math.random() * pokemonCount) + 1);
                 let random = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeID}/`)
                 let random2 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeID2}/`)
-                let data = document.querySelector("#data");
-                let info = document.createElement("P")
-                let info2 = document.createElement("P")
+                let challengerinfo = document.querySelector("#challengerinfo");
+                let championinfo = document.querySelector("#championinfo");
+                let info = document.createElement("li")
+                let info2 = document.createElement("li")
                 info.innerText = random.data.name;
                 info2.innerText = random2.data.name;
-                data.appendChild(info)
-                data.appendChild(info2)
+                challengerinfo.appendChild(info)
+                championinfo.appendChild(info2)
                 // let moves = await axios.get("https://pokeapi.co/api/v2/move")
             } 
             catch(err){
