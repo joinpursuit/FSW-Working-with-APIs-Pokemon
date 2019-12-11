e15ed7cd07c939983541979b3f5708bdc2a4eb06
Kevin Brutus <kevinbrutus@pursuit.org>
2019-12-10 20:51:11 -0500

    attach hp to pokemon

--- index.js
+++ index.js
@@ -12,26 +12,33 @@ document.addEventListener("DOMContentLoaded", () => {
                 let pokeID = Math.floor((Math.random() * pokemonCount) + 1);
                 let random = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeID}/`)
                 let challengerinfo = document.querySelector("#challengerinfo");
-                let info = document.createElement("li")
+                let info = document.createElement("h1")
                 let sprite1 = document.createElement("img")
                 info.innerText = random.data.name;
                 let src = random.data.sprites.front_default
                 sprite1.src = src
+                let challbase = document.createElement("li")
+                challbase.innerText = random.data.stats[0]["base_stat"]
+                debugger
                 challengerinfo.appendChild(info)
                 challengerinfo.appendChild(sprite1)
+                challengerinfo.appendChild(challbase)
                 
                 
                 // POKEMON 2
                 let pokeID2 = Math.floor((Math.random() * pokemonCount) + 1);
                 let random2 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeID2}/`)
                 let championinfo = document.querySelector("#championinfo");
+                let info2 = document.createElement("h1")
                 let sprite2 = document.createElement("img")
-                let info2 = document.createElement("li")
                 let src2 = random2.data.sprites.front_default
                 sprite2.src = src2
                 info2.innerText = random2.data.name;
+                let champbase = document.createElement("li")
+                champbase.innerText = random2.data.stats[0]["base_stat"]
                 championinfo.appendChild(info2)
                 championinfo.appendChild(sprite2)
+                championinfo.appendChild(champbase)
                 // let moves = await axios.get("https://pokeapi.co/api/v2/move")
             } 
             catch(err){
