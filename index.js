67c849f26b805c784d4506893ed3a161fd5360ae
bsihame <sihamebazi@pursuit.org>
2019-12-11 18:00:38 -0500

    display HP

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ document.addEventListener("DOMContentLoaded", async() => {
   let btn = document.querySelector("#getPoke")
     btn.addEventListener("click", () =>{
     getPokemon();
+    getMoves()
   })
   const getPokemon = async () => {
     let randomPokemon;
     let num = Math.floor(Math.random() * 809) + 1
-    console.log(num)
+    
     try {
       randomPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
       data = randomPokemon.data
@@ -19,30 +20,32 @@ document.addEventListener("DOMContentLoaded", async() => {
       h4.innerText = data.name;
       let displayName = document.querySelector(".data");
       displayName.appendChild(h4);
-    
+      
       let picture = document.createElement("img");
       picture.src = data.sprites.front_default;
       let displayPicture = document.querySelector(".data");
       displayPicture.appendChild(picture);
       
       let li = document.createElement("li");
+      li.id = "hp"
       li.innerText = "HP: "+ data.stats[5].base_stat
       let ul = document.createElement("ul");
       ul.appendChild(li); 
       let displayHp = document.querySelector(".data");
       displayHp.appendChild(ul);
-       
-      moves();
-
+      
+      
+      
       debugger 
     } catch (err) {  
       console.log('there', err)
     }
-    const moves = async () => {
-      
-    }
     // return randomPokemon.data
   }
+  // const moves = async () => {
+  //   try{
+  //     let getHP
+  //   }}
 
   pokemon1 = await getPokemon();
   pokemon2 = await getPokemon();
