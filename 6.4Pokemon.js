3f6b90fe45799b31d72dc2ef62901191e11edc81
DejaLFlynn <dejaflynn@pursuit.org>
2019-12-10 21:21:15 -0500

    display two pokemon function

--- 6.4Pokemon.js
+++ 6.4Pokemon.js
@@ -1,62 +1,34 @@
 document.addEventListener("DOMContentLoaded",()=>{
     let getPokemon = document.querySelector("#getPokemon")
-    getPokemon.addEventListener("click",displayOne)
-    // let battle = document.querySelector("#battle")
-    // battle.addEventListener("click", battlePokemon)
+    getPokemon.addEventListener("click",displayTwo)
+    const displayOne = async()=>{
+        try{
+            let randomA = Math.floor(Math.random()* 964) +1
+            let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomA}`)
+            let pokemonA = res.data.results
+            pokemonA.forEach(pokemon =>{
+                let poke = document.createElement("h2")
+                poke.innerText = pokemon.name;
+                getPokemon.appendChild(poke)
+            })
+
+        }catch(err){
+            console.log("error")
+        }
+    }
+    const displayTwo=async()=>{
+        displayOne()
+        displayOne()
+    }
+    displayTwo()
+    getPokemon.addEventListener("click", (event)=>{
+
+    })
+
 })  
 
-const displayOne = async()=>{
-    let sprite = document.createElement("img")
-    let randomA = Math.floor(Math.random()* 964) +1
-    let one = `https://pokeapi.co/api/v2/pokemon/${randomA}`
 
-
-    await axios.get(one).then((response)=>{
-        try{
-            let pokemonA = response.data
-            let stats = document.querySelector("pokemonA")
-            let name = document.createElement("h2")
-            let hp = document.createElement("h3")
-          
-            pokemonA.id = pokemonA.name
-            name.innerText = ""
-            // pokemonA.name
-            sprite.src = pokemonA.sprites.front_default
-            hp.innerText = pokemonA.stats[5].base_stat
-            pokemonA.appendChild(name)
-            pokemonA.appendChild(sprite)
-            pokemonA.appendChild(hp)
-            pokemonA.appendChild(stats)
-        }catch(err){
-            console.log("error press again")
-        }
-        displayOne()
-    })
-    
-}
-const displayTwo = async()=>{
-    let sprite = document.createElement("img")
-    let randomB = Math.floor(Math.random()*964)
-    let one = `https://pokeapi.co/api/v2/pokemon/${randomB}`
-    await axios.get(one).then((response)=>{
-        try{
-            let pokemonB = response.data
-            let stats = document.querySelector("PokemonB")
-            let name = document.createElement("h2")
-            let hp = document.createElement("h3")
-            pokemonB.id = pokemonB.name
-            name.innerText = ""
-            sprite.src = pokemonB.sprites.front_default
-            hp.innerText = pokemonB.stats[5].base_stat
-            pokemonB.appendChild(name)
-            pokemonB.appendChild(sprite)
-            pokemonB.appendChild(hp)
-            pokemonB.appendChild(stats)
-        }catch(err){
-            console.log("error press again")
-        }
-    })
-}
+  
 
 
 
@@ -66,9 +38,4 @@ const displayTwo = async()=>{
 
 
 
-// let url = `https://pokeapi.co/api/v2/pokemon/${one}`
-// let anotherUrl =  `https://pokeapi.co/api/v2/pokemon/${another}`
-// let first = document.getElementById("pokemonA")
-// let second = document.getElementById("pokemonB")
-// first.innerHTML = ""
-// second.innerHTML=""
+
