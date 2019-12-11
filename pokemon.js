16f2448f6a1168c2b8784387b43fde3c4a557810
Kevin Wong <kevinwong@pursuit.org>
2019-12-10 22:05:43 -0500

    complete project

--- pokemon.js
+++ pokemon.js
@@ -6,13 +6,16 @@ document.addEventListener("DOMContentLoaded", ()=> {
     data.appendChild(pokemon1);
     data.appendChild(pokemon2);
 
+    let pokeArr = []
+
     const getPokemon = async (pokemon) => {
         pokemon.innerHTML = "";
         let num = Math.floor(Math.random() * 807);
         try{
-            let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}/`);
+            let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
             let pokeData = res.data;
             let name = pokeData.name;
+            pokeArr.push(name)
             let sprite = pokeData.sprites.front_default;
             let hp = `HP: ${pokeData.stats[5]["base_stat"]}`;
 
@@ -21,8 +24,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
             let pokeHP = document.createElement("h3");
             let movesHeader = document.createElement("h3");
             
-
             pokeName.innerText = name;
+            pokeName.id = name;
             pokeSprite.src = sprite;
             pokeHP.innerText =  hp;
             movesHeader.innerText = "Moves:";
@@ -31,29 +34,17 @@ document.addEventListener("DOMContentLoaded", ()=> {
             pokemon.appendChild(pokeSprite);
             pokemon.appendChild(pokeHP);
             pokemon.appendChild(movesHeader);
+
             let shuffledMoves = shuffleArr(pokeData.moves).slice(1,5);
-            shuffledMoves.forEach((move) => {
+            shuffledMoves.forEach(async(move) => {
+                let moveData = await axios.get(move.move.url);
                 let li = document.createElement("li");
-                li.innerText = move.move.name;
+                li.innerText = `${move.move.name},   PP:${moveData.data.pp}`;
                 pokemon.appendChild(li);
-                let moveData = getMove(move);
-                let p = document.createElement("p");
-                p.innerText = moveData.pp;
-                pokemon.appendChild(p);
             })
         } catch (err){
             console.log(err)
             debugger
-        }
-    }
-    
-    const getMove = async (move) => {
-        try {
-            let res = await axios.get(move.move.url);
-            let moveData = res.data;
-            return moveData;
-        } catch (err)  {
-            console.log(err);
         }
     }
 
@@ -62,13 +53,31 @@ document.addEventListener("DOMContentLoaded", ()=> {
         getPokemon(pokemon2);
     })
 
-    let fight = document.querySelector("#battlePokemon");
-    fight.addEventListener("click", () => {
+    const battle = () => {
+        let battleHistory = document.querySelector(".battleHistory");
+        let shuffledPokemon = shuffleArr(pokeArr)
+
+        let pokeName1 = shuffledPokemon[0];
+        let pokeName2 = shuffledPokemon[1];
+
+        let h4 = document.createElement("h4");
+        h4.innerText += `${pokeName1} has defeated ${pokeName2}`;
+
+        document.querySelector("#theme").play();
         let memeDiv = document.querySelector(".meme");
         memeDiv.innerHTML = "";
         let meme = document.createElement("img");
         meme.src = "./extraStuff/pokeMeme.gif"
         memeDiv.appendChild(meme);
+        
+        battleHistory.appendChild(h4);
+        pokeArr = [];
+    
+    }
+
+    let fight = document.querySelector("#battlePokemon");
+    fight.addEventListener("click", () => {
+        battle(pokemon1, pokemon2);      
     })
 })
 
