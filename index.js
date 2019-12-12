0a9f81ef1dace5d7c2907ac4627b6e26dab3a607
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-12-12 00:17:02 -0500

    fixed order of stats, fixed info to display below button

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => { 
-    let select = document.querySelector("select");
+    // let select = document.querySelector("select");
     let button1 = document.querySelector("#button1");
     const choosePokemon = async () => { 
         try{
             let randomNum = Math.floor(Math.random() * 807)
             let pokemon1 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
-        // let pokemon2 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum(1, 809)}`);
-        // console.log(pokemon1)
+        
         let info = pokemon1.data;
         let poke = info.name;
         let picture = info.sprites.front_default;
@@ -14,18 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
         
         for(let i = 0; i < 4; i++) {
             
-            let mainPoke = document.querySelector("#mainPoke")
-            let getMoves = info.moves[i].move.name
-        let p1 = document.createElement("p")
-
-        p1.innerText = getMoves
-        mainPoke.appendChild(p1)}
-
-        let getMoves = info.moves
-        let pp = info.moves
-        debugger
-        
-
+        let mainPoke = document.querySelector("#mainPoke")
+        let getMoves = info.moves[i].move.name
+        let p4 = document.createElement("p")
+        p4.innerText = getMoves
+        mainPoke.appendChild(p4)}
         let mainPoke = document.querySelector("#mainPoke")
         let p1 = document.createElement("p")
         p1.innerText = poke
@@ -36,23 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
         let p3 = document.createElement("p")
         p3.innerText = hp 
         mainPoke.appendChild(p3)
-
-
-
-
-
-        // let displayName = document.createElement("body")
-        // let picture = document.createElement("body")
-        // let hp = document.createElement("body")
-
-
-
-
-
-        // ul.appendChild(li)
-        // debugger;
-
-    
     } catch(err) {
     console.log(err);
     }
@@ -61,11 +36,5 @@ document.addEventListener("DOMContentLoaded", () => {
    button1.addEventListener("click", () => {
        choosePokemon()
    })
-// choosePokemon()
-// })
-// let button1 = document.querySelector("#button1") {}
-// button1.addEventListener("click", () => {
-    //  choosePokemon(pokemon1);
-    //     choosePokemon(pokemon2)
 
  })
