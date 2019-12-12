ab30d46ad4de26dc2b657783d519f741a5911aa3
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-12-11 21:09:45 -0500

    complete createElements()

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 })
-const createElements = (pokemon) => {
+const createElements = async (pokemon) => {
     let ul = document.createElement("ul")
     let left = document.querySelector("#left")
+    let data = document.querySelector(".data");
+
     if(left){
         ul.id = "right"
     } else {
@@ -17,34 +19,56 @@ const createElements = (pokemon) => {
     ul.appendChild(name)
 
     let image = document.createElement("li")
+    let img = document.createElement("img");
+    img.src = pokemon.sprites.front_default;
+    image.appendChild(img);
+    ul.appendChild(image);
 
     let hp = document.createElement("li")
+    hp.innerText = `HP: ${pokemon.stats[5].base_stat}`;
+    ul.appendChild(hp);
 
     let moves = document.createElement("li")
+    moves.innerText = `Moves: `;
+    ul.appendChild(moves);
 
     let move1 = document.createElement("li")
+    let firstMove = await axios.get(pokemon.moves[0].move.url)
+    firstMove = firstMove.data;
+    move1.innerText = `${firstMove.name} PP:${firstMove.pp}`;
+    ul.appendChild(move1);
 
     let move2 = document.createElement("li")
+    let secondMove = await axios.get(pokemon.moves[1].move.url);
+    secondMove = secondMove.data;
+    move2.innerText = `${secondMove.name} PP:${secondMove.pp}`;
+    ul.appendChild(move2);
 
     let move3 = document.createElement("li")
+    let thirdMove = await axios.get(pokemon.moves[2].move.url);
+    thirdMove = thirdMove.data;
+    move3.innerText = `${thirdMove.name} PP:${thirdMove.pp}`
+    ul.appendChild(move3);
 
     let move4 = document.createElement("li")
-    
-    document.body.appendChild(ul)
+    let fourthMove = await axios.get(pokemon.moves[3].move.url);
+    fourthMove = fourthMove.data;
+    move4.innerText = `${fourthMove.name} PP:${fourthMove.pp}`;
+    ul.appendChild(move4);
+
+    data.appendChild(ul)
 
 }
-
-
  
 const getPokemon = async () => {
     try {
         // Fetching a random Pokemon
-        let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 964) + 1}`);
+        let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 807) + 1}`);
         // Initializing a pokemonOne for all information about the generated Pokemon
         let pokemonOne = res.data;
 
         // Repeat above for pokemonTwo
-        let resTwo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 964) + 1}`);
+        let resTwo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 807) + 1}`);
         let pokemonTwo = resTwo.data;
 
         // Create the Elements for pokemonOne
