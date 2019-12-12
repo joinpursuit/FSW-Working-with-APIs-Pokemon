3fc99676080c436297b01f3217a61f2e64bf9113
maria liza <mariamartinez@pursuit.org>
2019-12-12 02:57:07 -0500

    make code run to show both pokemon with name, sprite, and data, in columns side-by-side

--- pokemon.js
+++ pokemon.js
@@ -1,71 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   let button = document.querySelector("#getPokemon")
-  let pokemon1info = document.querySelector(".pokemon1info")
-  let pokemon2info = document.querySelector(".pokemon2info")
-
-  const getPokemon1 = async element => {
+  // let pokemonInfo = document.querySelector(".pokemon-info")
+  let poke1 = document.querySelector("#poke1")
+  let poke2 = document.querySelector("#poke2")
+  let p1 = document.querySelector("#p1")
+  let p2 = document.querySelector("#p2")
+  const getPokemon = async (pokemon, para) => {
     try {
       let num = Math.floor(Math.random() * 807)
       let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
-      // debugger
-      let character1 = res.data
-      let name1 = character1.name
-      let pokemonOne = document.querySelector("p")
-      pokemonOne.innerText = name1
+      let data = res.data
+      para.innerText = data.name;
       let img = document.createElement("img")
-      img.src = res.data.sprites.front_default
-      pokemonOne.appendChild(img)
-      let moves = res.data.moves.slice(0, 4)
-      // debugger
-      let ul = document.createElement("ul")
-      element.appendChild(ul)
-      moves.forEach(async (move, i) => {
-        let moveDeets2 = await axios.get(move.move.url)
-        // debugger;
+      img.src = data.sprites.front_default
+      pokemon.appendChild(img)
+      let moves = data.moves.slice(0, 4)
+      pokemon.appendChild(document.createElement("ul"))
+      moves.forEach(async move => {
+        let moveDeets = await axios.get(move.move.url)
         let li = document.createElement("li")
-        li.innerText = moveDeets2.data.name + " PP: " + moveDeets2.data.pp
-        element.appendChild(li)
+        li.innerText = moveDeets.data.name + " PP: " + moveDeets.data.pp
+        pokemon.appendChild(li)
       })
     } catch (err) {
       console.log("ERROR")
-      // debugger
-    }
-  }
-
-  const getPokemon2 = async () => {
-    try {
-      let num = Math.floor(Math.random() * 807)
-      let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
-      debugger
-      let character2 = res.data
-      let name2 = character2.name
-      let pokemonTwo = document.querySelector("p")
-      pokemonTwo.innerText = name2
-      let img2 = document.createElement("img2")
-      img2.src = res.data.sprites.front_default
-      pokemontwo.appendChild(img)
-      let moves2 = res.data.moves.slice(0, 4)
-      // debugger
-      let ul = document.createElement("ul")
-      pokemon2info.appendChild(ul)
-      moves2.forEach(async (move, i) => {
-        let moveDeets2 = await axios.get(move.move.url)
-        // debugger;
-        let li = document.createElement("li")
-        li.innerText = moveDeets2.data.name + " PP: " + moveDeet2.data.pp
-        ul.appendChild(li)
-      })
-    } catch (err) {
-      console.log("ERROR")
-      // debugger
     }
   }
 
   button.addEventListener("click", () => {
-    let pokemonOne = document.querySelector(".pokemon1info")
-    let pokemonTwo = document.querySelector(".pokemon2info")
-
-    getPokemon1(pokemonOne)
-    getPokemon1(pokemonTwo)
+    getPokemon(poke1, p1)
+    getPokemon(poke2, p2)
   })
 })
