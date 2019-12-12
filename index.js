82a21dadc3527c91d4290ff9efce051a848b04b2
Ohidurr <Ohidurrahman96@gmail.com>
2019-12-11 23:07:03 -0500

    pokemon name is displayed on webpage

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 document.addEventListener("DOMContentLoaded",() => {
-    let pokeData = Math.floor((Math.random()*809)+1) 
     let  pokeButton = document.querySelector("#get")
     
     const getPokemon = async () => {
+        let pokeData = Math.floor((Math.random()*809)+1) 
         try{
             let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeData}`)
-            let pokemon = res.data;
+            return res.data;
+            debugger
+            // let pokeName = await axios.get('https://pokeapi.co/api/v2/pokemon/' )
             // pokemon.forEach(pokemon => {
-            //     debugger 
+                
             // })
+            
+            
         }catch(err){
             console.log("Error");
             
         }
-    }
-    pokeButton.addEventListener("click",()=>{
+        
 
-        getPokemon()
-        getPokemon()
+        
+        
+    }
+    pokeButton.addEventListener("click", async()=>{
+       
+        let pokemon1 = await getPokemon()
+        let pokemon2 = await getPokemon()
+        // console.log(getPokemon());
+        
+        let ul = document.querySelector("ul")
+        let h1 = document.createElement("h1")
+        h1.innerText =  pokemon1.name
+        let newh1 = document.createElement("h1")
+
+        newh1.innerText =  pokemon2.name
+        document.body.appendChild(h1)
+        document.body.appendChild(newh1)
+        
+    
+        
     })
 })
 
