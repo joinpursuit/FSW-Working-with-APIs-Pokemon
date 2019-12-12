5a8d3f4a5d9d71488d0be41606f6739f798eeed6
bsihame <sihamebazi@pursuit.org>
2019-12-12 08:39:14 -0500

    Add refresh the pokemon

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", async() => {
   let pokemon1;
   let pokemon2;
-  let data;
-  //let picture;
   let btn = document.querySelector("#getPoke")
-    btn.addEventListener("click", () =>{
-    getPokemon();
-    getMoves()
+  btn.addEventListener("click", () =>{
+    pokemon1 = getPokemon();
+    pokemon2 = getPokemon();
+    let refresh = document.querySelector(".data");
+    refresh.innerHTML = ""
+      //getPokemon();
+      // getMoves()
+     
   })
+
+})
   const getPokemon = async () => {
     let randomPokemon;
     let num = Math.floor(Math.random() * 809) + 1
@@ -17,37 +22,44 @@ document.addEventListener("DOMContentLoaded", async() => {
       data = randomPokemon.data
       
       let h4 = document.createElement("h4");
-      h4.innerText = data.name;
       let displayName = document.querySelector(".data");
+      h4.innerText = data.name;
       displayName.appendChild(h4);
       
       let picture = document.createElement("img");
-      picture.src = data.sprites.front_default;
       let displayPicture = document.querySelector(".data");
+      picture.src = data.sprites.front_default;
       displayPicture.appendChild(picture);
       
       let li = document.createElement("li");
+      let ul = document.createElement("ul");
+      let displayHp = document.querySelector(".data");
       li.id = "hp"
       li.innerText = "HP: "+ data.stats[5].base_stat
-      let ul = document.createElement("ul");
       ul.appendChild(li); 
-      let displayHp = document.querySelector(".data");
       displayHp.appendChild(ul);
       
+      for(let i = 0; i <= 3 ; i ++){
+        let getMovesUrl = data.moves[i].move.url
+        let moves = await axios.get(getMovesUrl)
+       // debugger
+        let li = document.createElement("li");
+        li.innerText = "Moves:\n " + moves.data.name + " " + "PP: " +  moves.data.PP
+        ul.appendChild(li)
+      }
+    
       
-      
-      debugger 
+   //debugger 
     } catch (err) {  
       console.log('there', err)
     }
-    // return randomPokemon.data
+   
   }
-  // const moves = async () => {
-  //   try{
-  //     let getHP
-  //   }}
 
-  pokemon1 = await getPokemon();
-  pokemon2 = await getPokemon();
+  
+  // pokemon1 = await getPokemon();
+  // pokemon2 = await getPokemon();
+  
 
-})
+
+
