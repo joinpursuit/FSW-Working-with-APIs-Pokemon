4652dbcd697c95f7c528367c1fc91755157fb83b
maria liza <mariamartinez@pursuit.org>
2019-12-11 21:15:04 -0500

    add moves and pp to 1st pokemon

--- pokemon.js
+++ pokemon.js
@@ -1,33 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
   let button = document.querySelector("#getPokemon")
-//   let pokemonOne = document.createElement("p1")
-//   let pokemonTwo = document.createElement("p2")
+  let pokemon1info = document.querySelector(".pokemon1info")
+  //   let pokemonOne = document.createElement("p1")
+  //   let pokemonTwo = document.createElement("p2")
   const getPokemon = async () => {
     try {
       let num = Math.floor(Math.random() * 807)
       let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
-    //   debugger
+      // debugger
       let character = res.data
       let name1 = character.name
-      let pokemonOne = document.querySelector("p");
+      let pokemonOne = document.querySelector("p")
       pokemonOne.innerText = name1
       let img = document.createElement("img")
       img.src = res.data.sprites.front_default
-    //   let sprite = pokemon.sprites.front_default
-    pokemonOne.appendChild(img);
-      
+      pokemonOne.appendChild(img)
+      let moves = res.data.moves.slice(0, 4)
+      // debugger
+      let ul = document.createElement("ul")
+      pokemon1info.appendChild(ul);
+      moves.forEach(async (move, i) => {
+        let moveDeets = await axios.get(move.move.url)
+        // debugger;
+        let li = document.createElement("li")
+        li.innerText = moveDeets.data.name + " PP: " + moveDeets.data.pp
+        ul.appendChild(li)
+
+      })
+
     } catch (err) {
       console.log("ERROR")
-    //   debugger
+      // debugger
     }
   }
 
   button.addEventListener("click", () => {
     getPokemon()
 
-    
   })
-
-
 })
-
