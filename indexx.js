2dc5b345046a0ff8ad6db26d3bba8f766a4cd2d7
Danielle R Cherry <daniellecherry@pursuit.org>
2019-12-11 22:57:39 -0500

    WIP SUbmit

--- indexx.js
+++ indexx.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let floatLeft = document.querySelector(".pokemon1")
+  let floatRight = document.querySelector(".pokemon2")
   let firstPokemonUl = document.createElement("ul")
   let secondPokemonUl = document.createElement("ul")
+  // floatLeft.appendChild(firstPokemonUl)
 
   // at bottom add the 
 
@@ -10,38 +13,37 @@ const getPokemon = async () => {
         let pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`);
       
         let pokemonInfo = pokemon.data;
-        let battlerName = pokemonInfo.name;
-        let battlerPic = pokemonInfo.sprites.front_default;
-        let battlerHP = pokemonInfo.stats[5].base_stat;
-        
-        // let battlerMove = pokemonInfo.move;
-        // let movePP = pokemonInfo
-        // res.data.moves.slice
-        // // then take the url and grab the info from it  make a new url
-
-        
-        
+        let pokemonName = pokemonInfo.name;
+        let h1 = document.createElement("h1")
+        h1.innerText = pokemonName;
+        floatLeft.appendChild(h1);
 
 
-        
+        let pokemonPicUrl = pokemonInfo.sprites.front_default;
+        let pokemonPic = document.createElement("img");//same as line 17
+        pokemonPic.src = pokemonPicUrl
+        floatLeft.appendChild(pokemonPic);
+
+
+        let pokemonHP = pokemonInfo.stats[5].base_stat;
+        let p = document.createElement("p");
+        p.innerText = ` HP : ${pokemonHP}` ;
+        floatLeft.appendChild(p)
+
+        floatLeft.appendChild(firstPokemonUl);
+        firstPokemonUl.innerHTML;
+
+        let pokemonMove = pokemonInfo.move;
+        let movePP = pokemonInfo
+        res.data.moves.slice
+        // then take the url and grab the info from it  make a new url
+
+
       }catch(error) {
         console.log("This is the " + error)
       }
     }
-// const getPokemon2 = async () => {
-//       try {
-//         let randomNum = Math.floor(Math.random() * 807)
-//         let pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`);
 
-//         let battlerName = pokemon.data.name;
-//         let battlerPic = pokemon.data.sprites.front_default;
-//         let battlerHP = pokemon.data.stats[5];
-      
-        
-//       }catch(error) {
-//         console.log("This is the " + error)
-//       }
-//     }
      let button = document.querySelector("#getPoke");
         button.addEventListener("click", () => {
           getPokemon()
