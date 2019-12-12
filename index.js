d65597084ac8b9f93f3fd3c5362a6743d7683cb0
BrandonBrown <brandonbrown@pursuit.org>
2019-12-11 22:00:57 -0500

    save

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let li = document.createElement("li")
     let li2 = document.createElement("li")
 
+    let ul = document.createElement("ul")
+
     let pokebutton = document.getElementById("getPoke")
     // debugger
 
@@ -13,45 +15,67 @@ document.addEventListener("DOMContentLoaded", () => {
     })
      
 
-
+// axios.get each el and append it to the body then call function twice
     const getPokemon = async () => {
         
-        let res = await axios.get("https://pokeapi.co/api/v2/pokemon/")
-        // debugger
-        let count = parseInt(res.data.count)
+        // let res = await axios.get("https://pokeapi.co/api/v2/pokemon/")
+        // // debugger
+        // let count = parseInt(res.data.count)
         let pokeNum = Math.floor(Math.random()* 800)
         let pokeNum2 = Math.floor(Math.random()* 800)
 
         
-        let pokeImg1 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`)  //to get random sprite(pokemon imgage)
-        let pokeImg2 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeNum2}`)
-        
-        let pokeArr =[pokeImg1,pokeImg2]
-
-        let name1 = pokeImg1.data.name // to get name and save to a variable
-        let name2 = pokeImg1.data.name
-        
-        let hp1 = pokeImg1.data.stats.forEach((el, i) => { //finds the hp and saves it to a variable
-            if(i === pokeImg1.data.stats.length-1){
-                return el.base_stat
-            }
-        })
-        let hp2 = pokeImg2.data.stats.forEach((el, i) => {
-            if(i === pokeImg2.data.stats.length-1){
-                return el.base_stat
-            }
-        })
+        let pokemon1 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`)  //to get random sprite(pokemon imgage)
+        let pokemon2 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeNum2}`)
         // debugger
         
-        // console.log(res1)
-        img.src = pokeImg1.data.sprites.front_default
-        img2.src = pokeImg2.data.sprites.front_default
+        let pokeArr =[pokemon1,pokemon2]
 
-        for(let i = 0; i < pokeArr.length; i++){
-            if(i === 0){
-                
+        let name1 = pokemon1.data.name // to get name and save to a variable
+        let name2 = pokemon1.data.name
+        
+        let hp1 = pokemon1.data.stats.forEach((el, i) => { //finds the hp and saves it to a variable
+            if(i === pokemon1.data.stats.length-1){
+                return el.base_stat
             }
+        })
+        let hp2 = pokemon2.data.stats.forEach((el, i) => {
+            if(i === pokemon2.data.stats.length-1){
+                return el.base_stat
+            }
+        })
+        
+        for(let i = 0; i < 4; i++){
+            let result1 = []
+            let result2 = []
+
+            let moveName1 = pokemon1.data.moves[i].move.name
+            let moveName2 = pokemon2.data.moves[i].move.name
+
+            let pp1 = pokemon1.data.moves[i].move.url
+            let pp2 = pokemon2.data.moves[i].move.url
+
+            // let getPP = await axios.get(`https://pokeapi.co/api/v2/move/${i}/`)
+            let getPP = await axios.get(pp)
+            result1.push(moveName1 + "PP: ", )
+            result2.push()
+            getPP.data.pp
+
+            debugger
         }
+        
+        
+        
+        
+        // console.log(res1)
+        img.src = pokemon1.data.sprites.front_default
+        img2.src = pokemon2.data.sprites.front_default
+
+        // for(let i = 0; i < pokeArr.length; i++){
+        //     if(pokeArr[i]){
+                
+        //     }
+        // }
         
         // console.log(img.src)
         li.innerText = name1
