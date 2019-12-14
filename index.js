8deca3076ca446ec3c1dab85102f1f4aafe3dfbb
ashya <ashyamanning@pursuit.org>
2019-12-13 21:30:48 -0500

    append li to ul in getMoves

--- index.js
+++ index.js
@@ -12,27 +12,38 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
     const getMoves = (pokemon, div) => {
-        for (let i = 0; i < 4; i++) {
-            console.log(pokemon.moves[Math.floor(Math.random() * pokemon.moves.length)].move);
-            debugger
+        let foo = document.querySelector(`#${div.id} ul`);
+        console.log(foo);
+        if (foo) {
+            foo.parentNode.removeChild(foo);
         }
-
-
+        let ul = document.createElement("ul");
+        ul.id = "pokemonBattle";
+        div.appendChild(ul);
+        for (let i = 0; i < 4; i++) {
+            let randomMove = pokemon.moves[Math.floor(Math.random() * pokemon.moves.length - 1)].move.name;
+            let li = document.createElement("li");
+            li.innerText = randomMove;
+            ul.appendChild(li);
+        }
     }
     pokiBtn.addEventListener("click", async () => {
         let pokemon1 = await getPokemon();
         let pokemon2 = await getPokemon();
-        let div1 = document.createElement("div1");
-        let div2 = document.createElement("div2");
+        let div1 = document.createElement("div");
+        let div2 = document.createElement("div");
+        div1.id = "pokemon1";
+        div2.id = "pokemon2";
         document.body.appendChild(div1);
         document.body.appendChild(div2);
         console.log(pokemon1.name);
-        console.log(pokemon1.sprites);
-        console.log(pokemon1.stats[Math.floor(Math.random()* (pokemon1.stats).length - 1)])
+        console.log(pokemon1.sprites.front_default);
+        console.log(pokemon1.stats[Math.floor(Math.random()* pokemon1.stats.length - 1)])
+        console.log(pokemon1)
         console.log(getMoves(pokemon1, div1));
         console.log(pokemon2.name);
-        console.log(pokemon2.sprites);
-        console.log(pokemon2.stats[Math.floor(Math.random()* (pokemon2.stats).length - 1)])
+        console.log(pokemon2.sprites.front_default);
+        console.log(pokemon2.stats[Math.floor(Math.random()* pokemon2.stats.length - 1)])
         console.log(getMoves(pokemon2, div2));
     })
 })
