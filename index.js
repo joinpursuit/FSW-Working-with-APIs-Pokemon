0a7200fc53ed5af89b9c89d61a05ae0e154e8029
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-12-14 17:21:44 -0500

    printing 2 pokemon

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
-document.addEventListener("DOMContentLoaded", () => { 
+document.addEventListener("DOMContentLoaded", () => {
     let getPokeButton = document.querySelector("#getPokeButton");
-    const choosePokemon = async () => { 
-        try{
-        let randomNum = Math.floor(Math.random() * 807)
-        let pokemon1 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
-        let info = pokemon1.data;
-        let poke = info.name;
-        let picture = info.sprites.front_default;
-        let hp = `HP: ${info.stats[5].base_stat}`;
-        let dataBox = document.querySelector("#dataBox")
-        let p1 = document.createElement("p")
-        p1.innerText = poke
-        dataBox.appendChild(p1)
-        let p2 = document.createElement("img")
-        p2.src = picture
-        dataBox.appendChild(p2)
-        let p3 = document.createElement("p")
-        p3.innerText = hp 
-        dataBox.appendChild(p3)
+    const choosePokemon1 = async () => {
+        const div = document.createElement('div');
+        let dataBox = document.querySelector("#dataBox");
 
-        for(let i = 0; i < 4; i++) {
-            let dataBox = document.querySelector("#dataBox")
-            let moveName = info.moves[i].move.name
-            let moveUrl = info.moves[i].move.url
-            let pp = await axios.get(moveUrl)
-            let p5 = document.createElement("p")
-            p5.innerText = `Move: ${pp.data.name}; PP: ${pp.data.pp}`
-            dataBox.appendChild(p5)
+        try {
+            let randomNum = Math.floor(Math.random() * 807);
+            let pokemon1 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`);
+            let info = pokemon1.data;
+            let poke = info.name;
+            let picture = info.sprites.front_default;
+            let hp = `HP: ${info.stats[5].base_stat}`;
+            let p1 = document.createElement("p");
+
+            p1.innerText = poke 
+            div.appendChild(p1);
+            let p2 = document.createElement("img");
+            p2.src = picture;
+            div.appendChild(p2);
+            let p3 = document.createElement("p");
+            p3.innerText = hp;
+            div.appendChild(p3);
+
+            for (let i = 0; i < 4; i++) {
+                let moveName = info.moves[i].move.name
+                let moveUrl = info.moves[i].move.url
+                let pp = await axios.get(moveUrl)
+                let p5 = document.createElement("p")
+                p5.innerText = `Move: ${pp.data.name}; PP: ${pp.data.pp}`
+                div.appendChild(p5)
+            }
+
+            dataBox.append(div);
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    getPokeButton.addEventListener("click", () => {
+        document.getElementById('dataBox').innerHTML = '';
+        choosePokemon1()
+        choosePokemon1()
+    })
+    battleButton.addEventListener("click", () => {
+        // let battle = document.querySelector("dataBox")
+        const getWinner = (choosePokemon1) => {
+            let random = Math.floor(Math.random() * choosePokemon1())
         }
         
-        
-
-    } catch(err) {
-    console.log(err);
-    }
-   }
-
-   getPokeButton.addEventListener("click", () => {
-       choosePokemon()
-    //    choosePokemon()
-   })
- })
+    })
+})
