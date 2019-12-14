1c86dd7e6513395895426c3ede1ef01dad92e984
Phillip <phil.awich@gmail.com>
2019-12-14 13:16:16 -0500

    Add PP

--- index.js
+++ index.js
@@ -7,22 +7,35 @@ document.addEventListener("DOMContentLoaded" ,() =>{
             random = Math.floor(Math.random() * Math.floor(809))
             let res = await axios.get(`http://pokeapi.co/api/v2/pokemon/${random}/`)  
             let pokemonName = res.data.name
-            // debugger 
             let h1 = document.createElement("h1")
             h1.innerText = pokemonName
             pokemon1.appendChild(h1)
             
             let image = document.createElement("img")
-            image.src = res.data.sprites.front_default
+            image.src = res.data.sprites.back_default
             pokemon1.appendChild(image)
 
             let h2 = document.createElement("h2")
-            h2.innerText = res.data.stats[5].base_stat
+            h2.innerText = `HP: ${res.data.stats[5].base_stat}`
             pokemon1.appendChild(h2)
-
+            
+            let res2 = await axios.get(res.data.moves[0].move.url)
+            let res3 = await axios.get(res.data.moves[1].move.url)
+            let res4 = await axios.get(res.data.moves[2].move.url)
+            let res5 = await axios.get(res.data.moves[3].move.url)          
+            
             let h3 = document.createElement("h3")
-            h3.innerText = res.data.moves[0].move.name      
+            let h4 = document.createElement("h4")
+            let h5 = document.createElement("h5")
+            let h6 = document.createElement("h6")
+            h3.innerText = `${res.data.moves[0].move.name} PP: ${res2.data.pp}`
+            h4.innerText = `${res.data.moves[1].move.name} PP: ${res3.data.pp}`
+            h5.innerText = `${res.data.moves[2].move.name} PP: ${res4.data.pp}`
+            h6.innerText = `${res.data.moves[3].move.name} PP: ${res5.data.pp}`
             pokemon1.appendChild(h3)
+            pokemon1.appendChild(h4)
+            pokemon1.appendChild(h5)
+            pokemon1.appendChild(h6)
 
         } catch(error) {
             console.log("error")
@@ -34,7 +47,6 @@ document.addEventListener("DOMContentLoaded" ,() =>{
             random = Math.floor(Math.random() * Math.floor(964))
             let res = await axios.get(`http://pokeapi.co/api/v2/pokemon/${random}/`)
             let pokemonName = res.data.name
-
             let h1 = document.createElement("h1")
             h1.innerText = pokemonName
             pokemon2.appendChild(h1)
@@ -44,12 +56,26 @@ document.addEventListener("DOMContentLoaded" ,() =>{
             pokemon2.appendChild(image)
 
             let h2 = document.createElement("h2")
-            h2.innerText = res.data.stats[5].base_stat
+            h2.innerText = `HP: ${res.data.stats[5].base_stat}`
             pokemon2.appendChild(h2)
+
+            let res2 = await axios.get(res.data.moves[0].move.url)
+            let res3 = await axios.get(res.data.moves[1].move.url)
+            let res4 = await axios.get(res.data.moves[2].move.url)
+            let res5 = await axios.get(res.data.moves[3].move.url)
             
             let h3 = document.createElement("h3")
-            h3.innerText = res.data.moves[0].move.name
+            let h4 = document.createElement("h4")
+            let h5 = document.createElement("h5")
+            let h6 = document.createElement("h6")
+            h3.innerText = `${res.data.moves[0].move.name} PP: ${res2.data.pp}`
+            h4.innerText = `${res.data.moves[1].move.name} PP: ${res3.data.pp}`
+            h5.innerText = `${res.data.moves[2].move.name} PP: ${res4.data.pp}`
+            h6.innerText = `${res.data.moves[3].move.name} PP: ${res5.data.pp}`
             pokemon2.appendChild(h3)
+            pokemon2.appendChild(h4)
+            pokemon2.appendChild(h5)
+            pokemon2.appendChild(h6)
 
             // let image 
         } catch(error) {
